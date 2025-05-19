6622e8f01dc379bfd5c729e33a5fa5eea360d689
sondongian <77824725+sondongian@users.noreply.github.com>
2025-05-19 22:08:04 +0700

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,28 @@
+const admins = [
+  { username: "admin", password: "son123" },
+  { username: "mod", password: "123456" }
+];
+
+module.exports = async function handler(req, res) {
+  if (req.method === 'GET') {
+    return res.setHeader("Content-Type", "text/html").end(`
+      <html><head><title>Login</title></head><body>
+      <form method="POST">
+        <input name="username" placeholder="Username" />
+        <input name="password" type="password" placeholder="Password" />
+        <button>Login</button>
+      </form>
+      </body></html>
+    `);
+  }
+
+  const body = await new Promise(resolve => {
+    let data = "";
+    req.on("data", chunk => data += chunk);
+    req.on("end", () => resolve(new URLSearchParams(data)));
+  });
+
+  const username = body.get("username");
+  const password = body.get("password");
+
+  const ok = admins.some(admin => admin.username === userna
